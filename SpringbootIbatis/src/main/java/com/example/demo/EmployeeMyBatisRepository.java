d81e613359e16a27aa701a1b3574e7aee03353cd
hp <bindureddy4755@gmail.com>
2021-05-12 15:09:09 +0530

    initial commit

---
+++ SpringbootIbatis/src/main/java/com/example/demo/EmployeeMyBatisRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+import java.util.List;
+
+
+
+
+@Mapper
+public interface EmployeeMyBatisRepository {
+    @Select("select * from employees")
+    public List < Employee > findAll();
+
+    @Select("SELECT * FROM employees WHERE id = #{id}")
+    public Employee findById(long id);
+
+    @Delete("DELETE FROM employees WHERE id = #{id}")
+    public int deleteById(long id);
+
+    @Insert("INSERT INTO employees(id, first_name, last_name,email_address) " +
+        " VALUES (#{id}, #{firstName}, #{lastName}, #{emailId})")
+    public int insert(Employee employee);
+
+    @Update("Update employees set first_name=#{firstName}, " +
+        " last_name=#{lastName}, email_address=#{emailId} where id=#{id}")
+    public int update(Employee employee);
+}
---
+++ SpringbootIbatis/src/main/java/com/example/demo/SpringbootIbatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIbatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIbatisApplication.class, args);
+	}
+
+}
